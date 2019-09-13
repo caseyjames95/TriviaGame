7c1cd825b4e06961004749d67ad374dccbda0f60
caseyjames95 <caseypurves@gmail.com>
2019-09-12 20:50:52 -0700

    Basic Setup
    Directory setup for game

---
+++ assets/javascript/app.js
@@ -0,0 +1,48 @@
+const rule = {
+    list:
+}
+
+const car = {
+    image: ,
+    Question: "",
+    a: ,
+    b: ,
+    c: ,
+    d: ,
+}
+
+const car = {
+    image: ,
+    Question: "",
+    a: ,
+    b: ,
+    c: ,
+    d: ,
+}
+
+const car = {
+    image: ,
+    Question: "",
+    a: ,
+    b: ,
+    c: ,
+    d: ,
+}
+
+const car = {
+    image: ,
+    Question: "",
+    a: ,
+    b: ,
+    c: ,
+    d: ,
+}
+
+const car = {
+    image: ,
+    Question: "",
+    a: ,
+    b: ,
+    c: ,
+    d: ,
+}
